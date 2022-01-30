1f12405edfb130c9ff56702a0fd6ebbdb4c0e9ce
Donggun.Chung <chungdk1117@gmail.com>
2022-01-30 21:20:40 +0900

    :hammer: 2022.01.30.Sun [add Member RestController Advice]

---
+++ main/java/com/todo/exception/MemberExceptionHandler.java
@@ -0,0 +1,20 @@
+package com.todo.exception;
+
+import com.todo.api.MemberApiController;
+import org.springframework.core.Ordered;
+import org.springframework.core.annotation.Order;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice(basePackageClasses = {MemberApiController.class})
+@Order(Ordered.HIGHEST_PRECEDENCE)
+public class MemberExceptionHandler {
+
+    @ExceptionHandler
+    public ResponseEntity notFoundExceptionHandler() {
+
+        return new ResponseEntity(null, HttpStatus.OK);
+    }
+}
